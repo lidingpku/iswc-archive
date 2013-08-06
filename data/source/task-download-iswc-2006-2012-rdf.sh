2e2c0aa57db2195b2c818b5c3c193863a85cb9a1
Li Ding <lidingpku@gmail.com>
2013-08-06 11:38:59 -0700

    rename files in data/source, use consistant iswc-xxxx naming for all conferences

--- data/source/task-download-iswc-2006-2012-rdf.sh
+++ data/source/task-download-iswc-2006-2012-rdf.sh
@@ -1,7 +1,7 @@
 #/bin/bash!
 
 wget "http://data.semanticweb.org/dumps/conferences/iswc-2006-complete.rdf"
-wget "http://data.semanticweb.org/dumps/conferences/iswc-aswc-2007-complete.rdf" 
+wget "http://data.semanticweb.org/dumps/conferences/iswc-aswc-2007-complete.rdf" -O "iswc-2007-complete.rdf"
 wget "http://data.semanticweb.org/dumps/conferences/iswc-2008-complete.rdf" 
 wget "http://data.semanticweb.org/dumps/conferences/iswc-2009-complete.rdf" 
 wget "http://data.semanticweb.org/dumps/conferences/iswc-2010-complete.rdf" 
